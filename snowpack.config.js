fd9c2364c5a0bc8c0f3e68d51b656e40ec27cf41
xuhongbin <xuhongbin@yy.com>
2021-04-14 01:14:31 +0800

    feat: 增加静态资源

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,9 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    // '@snowpack/plugin-typescript',
+  ],
   packageOptions: {
     source: 'remote',
     types: true,
