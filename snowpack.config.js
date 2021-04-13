85d7b1be4a1147c2a38729aacb58e0c15462899c
xuhongbin <xuhongbin@yy.com>
2021-04-14 00:58:19 +0800

    feat: 实现0安装

---
+++ snowpack.config.js
@@ -0,0 +1,12 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
+  packageOptions: {
+    source: 'remote',
+    types: true,
+  },
+  mount: {
+    src: '/dist',
+    public: '/',
+  },
+}
